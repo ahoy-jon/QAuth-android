657d31540a61585ed856cec99f9442b773894344
Geoffroy Couprie <geo.couprie@gmail.com>
2012-12-01 21:39:48 +0100

    update the layout

--- gen/com/geal/lightningauth/R.java
+++ gen/com/geal/lightningauth/R.java
@@ -11,10 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int background_tile=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int logo=0x7f020003;
+        public static final int ombrebas=0x7f020004;
+        public static final int qr=0x7f020005;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int identitytext=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int qrbutton=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -25,7 +32,9 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int logo=0x7f040003;
         public static final int menu_settings=0x7f040002;
+        public static final int qr=0x7f040004;
     }
     public static final class style {
         /** 
@@ -54,5 +63,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int background=0x7f050002;
     }
 }
