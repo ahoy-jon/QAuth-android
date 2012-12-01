336b7f4a7c4b8b6d17e8e4bb27f7da8a8123d0de
Geoffroy Couprie <geo.couprie@gmail.com>
2012-12-01 21:27:25 +0100

    redirect to qr scanniong intent

--- src/com/geal/lightningauth/MainActivity.java
+++ src/com/geal/lightningauth/MainActivity.java
@@ -1,18 +1,30 @@
 package com.geal.lightningauth;
 
+import com.geal.lightningauth.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+	private ImageButton btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.v("qauth", "a");
+		btn = (ImageButton) findViewById(R.id.qrbutton);
+		Log.v("qauth", "b");
+        btn.setOnClickListener((OnClickListener) this);
+        Log.v("qauth", "c");
 	}
 
 	@Override
@@ -21,5 +33,11 @@ public class MainActivity extends Activity {
 		//getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	public void onClick(View view) {
+    	Log.v("qauth", "click");
+    	IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+      }
 
 }
